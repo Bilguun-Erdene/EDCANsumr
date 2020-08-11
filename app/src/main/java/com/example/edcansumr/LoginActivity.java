010765b2d3e073ffb6d53014fa34e5812d94c805
Bilguun-Erdene <66424530+Bilguun-Erdene@users.noreply.github.com>
2020-08-11 09:53:42 +0900

    #2

--- app/src/main/java/com/example/edcansumr/LoginActivity.java
+++ app/src/main/java/com/example/edcansumr/LoginActivity.java
@@ -3,13 +3,18 @@ package com.example.edcansumr;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.edcansumr.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private ActivityLoginBinding binding;
 
     @Override
@@ -22,8 +27,26 @@ public class LoginActivity extends AppCompatActivity {
         binding.btnLoginSignin.setOnClickListener(view -> {
             login(binding.getEmail(), binding.getPw());
         });
+        binding.btnLoginSignup.setOnClickListener(view -> {
+            startActivity(new Intent (LoginActivity.this, RegisterActivity.class));
+        });
     }
     private void login(String email, String pw){
-        Toast.makeText(this, email+" "+pw, Toast.LENGTH_SHORT).show();
+        firebaseFirestore
+            .collection("users")
+                .document()
+                .get()
+                .addOnSuccessListener(runnable -> {
+                    firebaseAuth
+                            .signInWithEmailAndPassword(email, pw)
+                            .addOnSuccessListener(runnable1 -> {
+                                //goto main
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            })
+                            .addOnFailureListener(e -> Toast.makeText(this, e.getLocalizedMessage(),Toast.LENGTH_SHORT));
+                })
+                .addOnFailureListener(e -> Toast.makeText(this, e.getLocalizedMessage(),Toast.LENGTH_SHORT));
+
     }
 }
--- app/src/main/java/com/example/edcansumr/RegisterActivity.java
+++ app/src/main/java/com/example/edcansumr/RegisterActivity.java
@@ -1,14 +1,59 @@
 package com.example.edcansumr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.edcansumr.databinding.ActivityLoginBinding;
+import com.example.edcansumr.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+        binding.setName("");
+        binding.setEmail("");
+        binding.setPw("");
+        binding.setPwcheck("");
+
+        binding.btnRegiSignup.setOnClickListener(view -> {
+            register(binding.getName(), binding.getEmail(), binding.getPw(), binding.getPwcheck());
+        });
+    }
+
+    private void register(String name, String email, String pw, String pwcheck){
+
+        if (name.isEmpty()||email.isEmpty()||pw.isEmpty()||pwcheck.isEmpty()){
+            Toast.makeText(this, "input empty spaces", Toast.LENGTH_SHORT).show();
+        }
+        if (!pw.equals(pwcheck)){
+            Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show();
+        }
+
+        firebaseFirestore
+                .collection("users")
+                .document(email)
+                .set(new UserModel(name, email))
+                .addOnSuccessListener(runnable -> {
+                    firebaseAuth
+                            .createUserWithEmailAndPassword(email, pw)
+                            .addOnSuccessListener(runnable1 -> {
+                                Toast.makeText(this, "정상적으로 가입되었습니다!", Toast.LENGTH_SHORT).show();
+                                finish();
+                            })
+                            .addOnFailureListener(e -> Toast.makeText(this, e.getLocalizedMessage(),Toast.LENGTH_SHORT));
+                })
+        .addOnFailureListener(e -> Toast.makeText(this, e.getLocalizedMessage(),Toast.LENGTH_SHORT));
     }
 }
